f8228263c781b9c7d2d7ec4242886795f9d51160
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-03-26 15:05:32 -0400

    Create fuction to repeat string and pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  let repeatedString = "";
+  while (num > 0) {
+    repeatedString += string;
+    num--;
+  }
+  if (num < 0) {
+    repeatedString = "ERROR";
+  }
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
