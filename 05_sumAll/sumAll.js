31d2bb5692b01c41c9431b9f6358b3f8395dd57a
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-04-01 15:40:37 -0400

    Create function to sum numbers
    Given two number, sum the two number including all numbers in between. Check for only positive numbers. No strings, arrays, or booleans. Sort the array using function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+  const numbers = [num1, num2];
+  let error = "ERROR";
+
+  if (num1 < 0 || num2 < 0 || typeof num1 != "number" ||  typeof num2 != "number") { 
+    return error;
+  } else {
+    let total = 0;
+    numbers.sort(function(a, b){return a-b});
+
+    for (let i = numbers[0]; i <= numbers[1]; i++) {
+      total += i;
+    }
+  
+    return total;
+  }
 
 };
 
