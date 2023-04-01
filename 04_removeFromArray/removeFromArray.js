45aed7ae54aab5bc50119267d7f8216a0f505871
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-04-01 11:30:16 -0400

    Create function to remove items from array
    Use indexOf() to find index of specifc values. Loop over the array splicing the values out of the array. Use continue to start the loop over when indexOf returns -1.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+  const arrayToEdit = arguments[0];
 
+  for(let i = 1; i < arguments.length; i++) {
+    const itemToRemove = arrayToEdit.indexOf(arguments[i]);
+    if(itemToRemove === -1) {continue; }
+    arrayToEdit.splice(itemToRemove,  1);
+  }
+
+  return(arrayToEdit);  
 };
 
 // Do not edit below this line
