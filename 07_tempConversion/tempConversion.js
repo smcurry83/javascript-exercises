b79ca5491679ce74933c9bbb48316786a26a5b18
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-04-18 12:23:19 -0400

    Create functions to converts temperatures
    Use formulas to convert C to F and F to C. Use toFixed for decimal rounding and parseFloat to convert back from string.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const temp = fahrenheit;
+  let celsiusTemp = 0;
+  
+  celsiusTemp = (temp - 32) * (5/9);
+
+  if (celsiusTemp % 1 != 0) {
+    return parseFloat(celsiusTemp.toFixed(1));
+  } else {
+    return celsiusTemp;
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const temp = celcius;
+  let fahrenheitTemp = 0;
+
+  fahrenheitTemp = (temp * (9/5)) + 32;
+  
+  if (fahrenheitTemp % 1 != 0) {
+    return parseFloat(fahrenheitTemp.toFixed(1));
+  } else {
+    return fahrenheitTemp;
+  }
 };
 
 // Do not edit below this line
